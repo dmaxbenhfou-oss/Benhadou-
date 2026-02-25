89a7ffab1826ba4e2c983670e66ec0c0ca911c27
dmaxbenhfou-oss <dmaxbenhfou@gmail.com>
2026-02-25 22:22:53 +0000

    Add Saving model class for tracking savings goals

---
+++ lib/models/saving.dart
@@ -0,0 +1,17 @@
+class Saving {
+  String id;
+  String title;
+  double targetAmount;
+  double currentAmount;
+  DateTime createdDate;
+  DateTime targetDate;
+
+  Saving({
+    required this.id,
+    required this.title,
+    required this.targetAmount,
+    required this.currentAmount,
+    required this.createdDate,
+    required this.targetDate,
+  });
+}
