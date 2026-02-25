1a91ea61b715059af1fd684e1f47eb32a6e4cef3
dmaxbenhfou-oss <dmaxbenhfou@gmail.com>
2026-02-25 22:20:27 +0000

    Create Expense model class

---
+++ lib/models/expense.dart
@@ -0,0 +1,39 @@
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final String category;
+  final DateTime date;
+  final String description;
+
+  Expense({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+    required this.description,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'category': category,
+      'date': date.toIso8601String(),
+      'description': description,
+    };
+  }
+
+  factory Expense.fromMap(Map<String, dynamic> map) {
+    return Expense(
+      id: map['id'],
+      title: map['title'],
+      amount: map['amount'],
+      category: map['category'],
+      date: DateTime.parse(map['date']),
+      description: map['description'],
+    );
+  }
+}
